26eecbb6a154bcb549250d852ba6d93f1100c2ee
waiyongc <101108549+waiyongc@users.noreply.github.com>
2022-07-16 15:41:10 +0800

    Update sentiment-analysis-app.py

--- AppCode/sentiment-analysis-app.py
+++ AppCode/sentiment-analysis-app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 import pickle 
-from xgboost import XGBClassifier
+import xgboost
 import matplotlib.pyplot as plt
 from plotly import express as px
 from emot.emo_unicode import UNICODE_EMOJI
